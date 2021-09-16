7ee4225d163677b43151150bcc1e2774074b90a0
Oleksandr Horbunov1 <oleksandr_horbunov1@epam.com>
2021-09-16 21:00:32 +0300

    hwm1.py pep-8 corrections

--- hwm1.py
+++ hwm1.py
@@ -13,14 +13,14 @@ sorted_list = []
 # While random list is not empty do
 while randomlist:
     # Variable min gets value of first list element
-    min = randomlist[0]
-    # Go through the random list and compare each number with vmin
+    min_value = randomlist[0]
+    # Go through the random list and compare each number with min
     for x in randomlist:
         # If some element is less than min
-        if x < min:
-            min = x           # Variable min gets value of element
-    sorted_list.append(min)   # Add min to sorted list
-    randomlist.remove(min)    # Remove element from random list
+        if x < min_value:
+            min_value = x  # Variable min gets value of element
+    sorted_list.append(min_value)  # Add min to sorted list
+    randomlist.remove(min_value)  # Remove element from random list
 
 # Output of sorted list
 print("Sorted list\n", sorted_list)
@@ -35,11 +35,11 @@ odd_count = 0
 for i in sorted_list:
     # Find even or odd number
     if i % 2 == 0:
-        even_sum += int(i) # increase even sum
-        even_count += 1    # increase even count
+        even_sum += int(i)  # increase even sum
+        even_count += 1  # increase even count
     else:
         odd_sum += int(i)  # increase odd sum
-        odd_count += 1     # increase odd count
+        odd_count += 1  # increase odd count
 
 # Print out averages for even and odd numbers
 print("Even Average: " + str(int(even_sum) / int(even_count)))
