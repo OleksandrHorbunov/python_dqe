4ccb921aba8035dc64bb69d8e17a7f89003a4750
Oleksandr Horbunov1 <oleksandr_horbunov1@epam.com>
2021-09-22 14:18:24 +0300

    Docstrings added, get_dicts() - added duplicates filtering, get_common_dict() - changed algoritm

--- hwm2.py
+++ hwm2.py
@@ -1,50 +1,64 @@
-# Import module random
 import random
 import string
 
 
-# create a list of random number of dicts (from 2 to 10)
 def get_dicts(number_dicts):
-    dict_list = []                                              # Create empty list
-    for i in range(number_dicts):                               # Go through the list of dicts
-        number = random.randint(1, number_dicts)                # number of keys and values in a dict
-        keys = random.sample(string.ascii_lowercase, number)    # random numbers of keys (letter)
-        values = random.sample(range(0, 100), number)           # random numbers of values (number)
-        dictionary = dict(zip(keys, values))                    # create dictionary
-        dict_list.append(dictionary)                            # add dict to list
-    print("List of dicts\n", dict_list)                                            # list of dicts
-    return dict_list                                            # function returns a list
+    # Return a list of random number of dicts (from 2 to 10)
+
+    # Parametrs:
+    #   number_dicts (int):  random number of dicts in a list
+    dict_list = []
+    for i in range(number_dicts):
+        number = random.randint(1, number_dicts)            # Dict size
+        key_list = []
+        key_counter = 0
+        while key_counter < number:                         # Check for letter duplicates
+            keys = random.choice(string.ascii_lowercase)
+            if keys not in key_list:
+                key_list.append(keys)
+                key_counter += 1
+        value_list = []
+        value_counter = 0
+        while value_counter < number:                          # Check for number duplicates
+            values = random.randint(0, 100)
+            if values not in value_list:
+                value_list.append(values)
+                value_counter += 1
+        dictionary = dict(zip(key_list, value_list))            # Creating a dict
+        dict_list.append(dictionary)                            # Adding a dict to a list
+    print("List of dicts\n", dict_list)
+    return dict_list
 
 
 # create one common dict
 def get_common_dict(dicts):
-    res = {}                                               # final dict
-    res1 = {}                                              # auxiliary dict to keep key and dict num
-    dn = 1                                                 # dict counter
-    for dic in dicts:                                      # go through list of dicts
-        for key in dic.keys():                             # for each key in dict
-            if key not in res.keys():                      # if key is not in final dict
-                res[key] = dic[key]                        # add key to final dict
-            if key in res.keys() and \
-                    dic[key] > res[key]:                   # if key value in final dict less then the same from another
-                res[key] = dic[key]                        # change value in final dict for key
-                res1[key] = dn                             # add key: dn to auxiliary dict to know key and dict number
-            elif key in res.keys() and \
-                    dic[key] < res[key]:                   # if key value in final dict more then the same from another
-                res1[key] = dn                             # change value for key
-        dn += 1                                            # increase counter
-    print("Common dict with the biggest value for key\n",
-          res)                                             # Print common dict
-    print("Auxiliary dict\n", res1)                        # Print auxiliary dict
-    for k in res1.keys():                                  # Go through auxiliary dict
-        new_key = k + "_" + str(res1[k])                   # Create a new key
-        res[new_key] = res.pop(k)                          # Create a new key in final dict with value
-    print("Final dict with new keys\n", res)               # Print final dict with updated keys
-    return res
+    # Return one common dict
+
+    # Parametrs:
+    #   dicts (list): get generated list of dicts
+    res = {}
+    dict_aux = {}       # Creating auxiliary dict to keep key and num of dict
+    for dict_order in range(len(dicts)):        # Go through the list of dicts
+        for dict_key in dicts[dict_order]:
+            if dict_key not in res:
+                res[dict_key] = dicts[dict_order][dict_key]     # Adding dict elen to res dict
+                for sub_dict_order in range(dict_order + 1, len(dicts)):        # Checking for the max key value
+                    if dict_key in dicts[sub_dict_order] and \
+                            dicts[sub_dict_order][dict_key] > dicts[dict_order][dict_key]:
+                        res[dict_key] = dicts[sub_dict_order][dict_key]         # Changing the value
+                        dict_aux[dict_key] = sub_dict_order + 1                 # Adding key and dict number
+                    elif dict_key in dicts[sub_dict_order] and \
+                            dicts[sub_dict_order][dict_key] < dicts[dict_order][dict_key]:
+                        dict_aux[dict_key] = dict_order + 1
+    print("Auxiliary dict\n", dict_aux)
+    for aux_key in dict_aux.keys():     # Changing key to new key with suffix of dict number
+        new_key = aux_key + "_" + str(dict_aux[aux_key])
+        res[new_key] = res.pop(aux_key)
+    print("Common dict\n", res)
 
 
 if __name__ == "__main__":
     # Generate random number of dicts
-    number_dicts = random.randint(2, 10)                   # Generate number of dicts
-    g_dict_list = get_dicts(number_dicts)                  # Call get_dicts
-    get_common_dict(g_dict_list)                           # Call get_common_dict
+    number_dicts = random.randint(2, 10)
+    g_dict_list = get_dicts(number_dicts)
+    get_common_dict(g_dict_list)
