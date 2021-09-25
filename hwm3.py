b2f899a97bba25eeef7c622c10a31a4dd32b786f
Oleksandr Horbunov1 <oleksandr_horbunov1@epam.com>
2021-09-25 12:27:34 +0300

    Added homework for module 3

---
+++ hwm3.py
@@ -0,0 +1,104 @@
+import re
+
+text = """homEwork:
+	tHis iz your homeWork, copy these Text to variable. 
+
+	You NEED TO normalize it fROM letter CASEs point oF View. also, create one MORE senTENCE witH LAST WoRDS of each existING SENtence and add it to the END OF this Paragraph.
+
+	it iZ misspeLLing here. fix“iZ” with correct “is”, but ONLY when it Iz a mistAKE. 
+
+	last iz TO calculate nuMber OF Whitespace characteRS in this Text. caREFULL, not only Spaces, but ALL whitespaces. I got 87.
+"""
+
+
+def text_split(text_orig):
+    # Returns list of centences
+    # Parameters:
+    # text_orig (str): Origin text
+
+    text_splited = text_orig.split("\n")
+    return text_splited
+
+
+def text_normalize(text_norm):
+    # Returns normalized text
+    # Parameters:
+    # text_norm (list): List of sentences
+
+    for text_sentence in range(len(text_norm)):
+        if text_norm[text_sentence].startswith("\t") == True:
+            text_norm[text_sentence] = text_norm[text_sentence].replace("\t", "")
+            text_norm[text_sentence] = text_norm[text_sentence].capitalize()
+            if text_norm[text_sentence].count(".") > 1:
+                sentence_splited = text_norm[text_sentence].split(".")
+                for i in range(len(sentence_splited)):
+                    if sentence_splited[i] != " " and sentence_splited[i] != "":
+                        sentence_splited[i] = sentence_splited[i].strip().capitalize() + "."
+                text_norm[text_sentence] = ' '.join(sentence_splited)
+                text_norm[text_sentence] = text_norm[text_sentence].strip()
+                text_norm[text_sentence] = "\t" + text_norm[text_sentence]
+            else:
+                text_norm[text_sentence] = text_norm[text_sentence].strip()
+                text_norm[text_sentence] = "\t" + text_norm[text_sentence]
+        else:
+            text_norm[text_sentence] = text_norm[text_sentence].capitalize()
+    return text_norm
+
+
+def more_setences(text_extended):
+    # Returns extended sentences with last words from each sentence
+    # Parametrs:
+    # text_extend (list): Normalized text
+
+    for ext_sentense in range(len(text_extended)):
+        if text_extended[ext_sentense].count(".") > 1:
+            sentence_splited = text_extended[ext_sentense].split(".")
+            ext_words = []
+            for i in range(len(sentence_splited)):
+                if sentence_splited[i] != " " and sentence_splited[i] != "":
+                    ext_words.append(sentence_splited[i].split(" ")[-1])
+            text_extended[ext_sentense] = '. '.join(sentence_splited)
+            for i in ext_words:
+                text_extended[ext_sentense] = text_extended[ext_sentense] + " " + i
+        else:
+            ext_word = text_extended[ext_sentense].split(" ")[-1]
+            text_extended[ext_sentense] = text_extended[ext_sentense] + " " + ext_word
+    return text_extended
+
+
+def text_misspell(text_misspelling):
+    # Returns corrected sentences
+    # Parameters:
+    # text_misspelling (list): Extended text
+
+    for misspel_sentense in range(len(text_misspelling)):
+        text_misspelling[misspel_sentense] = re.sub(r'\siz\s', ' is ', text_misspelling[misspel_sentense])
+        text_misspelling[misspel_sentense] = re.sub(r'\Wiz\W', '"is"', text_misspelling[misspel_sentense])
+    return text_misspelling
+
+
+def count_whitespaces(misspell_sentences):
+    # Returns number of whitespaces and final text
+    # Parametrs:
+    # misspell_sentences (list): corrected sentences
+
+    count = 0
+    text_result = ""
+    for ms in range(len(misspell_sentences)):
+        for i in misspell_sentences[ms]:
+            if (i.isspace()):
+                count += 1
+        text_result = text_result + misspell_sentences[ms] + "\n"
+    print("Number of whitespaces is:", count)
+    print("\nTransformed text is:\n\n", text_result)
+
+if __name__ == "__main__":
+    text_spliting = text_split(text)
+    # print("Splited text\n", text_spliting)
+    text_normalizing = text_normalize(text_spliting)
+    # print("Normalized text\n", text_normalizing)
+    extended_sentense = more_setences(text_normalizing)
+    # print(extended_sentense)
+    misspell_sentences = text_misspell(extended_sentense)
+    # print(misspell_sentences)
+    whitespaces = count_whitespaces(misspell_sentences)
